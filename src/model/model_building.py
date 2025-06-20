99c86fa246c30899132b6883c7e66eef56be6c89
datta-abhi <rookbee91@gmail.com>
2025-06-20 14:37:41 +0530

    model building and evaluation files added

---
+++ src/model/model_building.py
@@ -0,0 +1,26 @@
+
+import yaml
+import numpy as np
+import pandas as pd
+import joblib
+from sklearn.ensemble import GradientBoostingClassifier
+
+import warnings
+warnings.filterwarnings("ignore") 
+
+# fetching params from yaml file
+params = yaml.safe_load(open('params.yaml','r'))['model_building']
+# params = {'n_estimators': 50,
+#           'learning_rate': 0.01}
+
+# fetch training data from data/ features
+train_data = pd.read_csv('./data/features/train_bow.csv')
+
+X_train = train_data.iloc[:,0:-1].values
+y_train = train_data.iloc[:,-1].values
+
+# Create model object and fit train data
+clf = GradientBoostingClassifier(n_estimators=params['n_estimators'],learning_rate=params['learning_rate'])
+clf.fit(X_train,y_train)
+
+joblib.dump(clf,'sentiment_model.joblib')
---
+++ src/model/model_evaluation.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import joblib
+import json
+
+import warnings
+warnings.filterwarnings("ignore")
+
+from sklearn.metrics import accuracy_score,recall_score,roc_auc_score,precision_score
+
+# load saved model
+clf = joblib.load('sentiment_model.joblib')
+
+# load test data
+test_data = pd.read_csv('./data/features/test_bow.csv')
+
+X_test = test_data.iloc[:,0:-1].values
+y_test = test_data.iloc[:,-1].values
+
+# predictions using model
+y_pred = clf.predict(X_test)
+y_pred_proba = clf.predict_proba(X_test)[:,1]
+
+# Calculate evaluation metrics
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+auc = roc_auc_score(y_test, y_pred_proba)
+
+metrics_dict={
+    'accuracy': round(accuracy,4),
+    'precision':round(precision,4),
+    'recall': round(recall,4),
+    'auc': round(auc,4)
+}
+
+with open('metrics.json','w') as f:
+    json.dump(metrics_dict,f,indent=4)
