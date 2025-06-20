e5f7d8e3e3f8cba7206dce09fe13d7bb00c5243d
datta-abhi <rookbee91@gmail.com>
2025-06-20 14:29:50 +0530

    initial cookiecutter project setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='NLP project on classifying sentiment based on twitter posts. Emphasis is to display Best Practices for modular code development using MLOPs',
+    author='abhigyan_datta',
+    license='',
+)
